4fabb7e897dbd156954d0b55a921d7f614628d18
Андрей Миронов <anvlmironov@greenatom.ru>
2021-10-20 17:30:24 +0400

    added filtration

--- clientApp/src/app/app.module.ts
+++ clientApp/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FilterComponent } from './filter/filter.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent,
+    FilterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
